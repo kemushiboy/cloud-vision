dca4be871746cfed47dc458d991cdd43dec0eabb
Tatsuya_Ishikawa <Ishikawa@MacBook-Pro.local>
2016-05-09 00:52:44 +0900

    PC上では安定

--- vision.py
+++ vision.py
@@ -4,6 +4,7 @@ import base64
 import json
 
 import cv2
+import time
 
 from requests import Request, Session
 
@@ -26,38 +27,17 @@ str_api_key = "AIzaSyAh5zYBWfiPMDnt8GFPEye8fImsP7JStFs"
 str_headers = {'Content-Type': 'application/json'}
         
     
-# リクエスト送信
-#obj_session = Session()
-#obj_request = Request("POST",str_url + str_api_key,
-#                              data=json.dumps(str_json_data),
-#                              headers=str_headers
-#                              )
-#obj_prepped = obj_session.prepare_request(obj_request)
-#obj_response = obj_session.send(obj_prepped,verify=True,timeout=60)
-    
-# 分析結果の取得
-#if obj_response.status_code == 200:
-#    print obj_response.text
-#return obj_response.text
-#else:
-#return "error"
-#    print "error"
-
-
-
-
-#cv.NamedWindow("ClientCAM", 1)
-capture = cv2.VideoCapture(0)#キャプチャに使うカメラの選択
+capture = cv2.VideoCapture(0)
 while True:
     ret, frame = capture.read()
-    #img = cv2.QueryFrame(capture)#画像のキャプチャ
-    buf = cv2.cv.EncodeImage(".jpeg",cv2.cv.fromarray(frame)).tostring() 
-    str_encode_file = base64.b64encode(buf)
+    buf = cv2.cv.EncodeImage(".jpeg",cv2.cv.fromarray(frame)).tostring()#frameをjpeg文字列に変換
+    str_encode_file = base64.b64encode(buf)#jpeg文字列からバイナリに変換
     cv2.imshow('frame',frame)
-    if cv2.waitKey(10) == 27:
+    if cv2.waitKey(10) == 27:#escを押したら
+      break
         
       # Cloud Vision APIの仕様に沿ってJSONのペイロードを定義。
-      str_json_data = {
+    str_json_data = {
           'requests': [
                        {
                        'image': {
@@ -73,25 +53,34 @@ while True:
                        ]
                        }
 
-
     # リクエスト送信
-      obj_session = Session()
-      obj_request = Request("POST",str_url + str_api_key,
+    obj_session = Session()
+    obj_request = Request("POST",str_url + str_api_key,
                                         data=json.dumps(str_json_data),
                                         headers=str_headers
                                         )
-      obj_prepped = obj_session.prepare_request(obj_request)
-      obj_response = obj_session.send(obj_prepped,verify=True,timeout=60)
+    obj_prepped = obj_session.prepare_request(obj_request)
+    obj_response = obj_session.send(obj_prepped,verify=True,timeout=60)
           
       # 分析結果の取得
-      if obj_response.status_code == 200:
-        print obj_response.text
-        #return obj_response.text
-      else:
-        #return "error"
-        print "error"
+    if obj_response.status_code == 200:
+      print obj_response.text
+      jsonData = json.loads(obj_response.text)
+      params = jsonData["responses"][0]["safeSearchAnnotation"]
+      if params["adult"] == "VERY_LIKELY":
+        print "out"
+      if params["adult"] == "LIKELY":
+        print "out"
+      if params["adult"] == "POSSIBLE":
+        print "seout"
 
-      break
+        #return obj_response.text
+    else:
+      #return "error"
+      print "error"  
+
+    time.sleep(5)
+    #break
 
 capture.release()
 cv2.destroyAllWindows()#事後処理
