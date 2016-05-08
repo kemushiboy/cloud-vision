e833ac598f3f007f16031cdbb45156ae95b39bdf
Tatsuya_Ishikawa <Ishikawa@MacBook-Pro.local>
2016-05-09 01:41:35 +0900

    sleepを使わず、Timeを計測するように変更

--- vision.py
+++ vision.py
@@ -26,61 +26,68 @@ str_api_key = "AIzaSyAh5zYBWfiPMDnt8GFPEye8fImsP7JStFs"
 # Content-TypeをJSONに設定
 str_headers = {'Content-Type': 'application/json'}
         
+
     
 capture = cv2.VideoCapture(0)
+interval = 5.0
+bufTime = time.time()
+
 while True:
     ret, frame = capture.read()
-    buf = cv2.cv.EncodeImage(".jpeg",cv2.cv.fromarray(frame)).tostring()#frameをjpeg文字列に変換
-    str_encode_file = base64.b64encode(buf)#jpeg文字列からバイナリに変換
     cv2.imshow('frame',frame)
     if cv2.waitKey(10) == 27:#escを押したら
       break
-        
+    
+    if time.time() - bufTime >= interval:#intervalで指定した時間が経過したら
+      bufTime = time.time()
+
+      buf = cv2.cv.EncodeImage(".jpeg",cv2.cv.fromarray(frame)).tostring()#frameをjpeg文字列に変換
+      str_encode_file = base64.b64encode(buf)#jpeg文字列からバイナリに変換
+      
       # Cloud Vision APIの仕様に沿ってJSONのペイロードを定義。
-    str_json_data = {
-          'requests': [
-                       {
-                       'image': {
-                       'content': str_encode_file
-                       },
-                       'features': [
-                                    {
-                                    'type': "SAFE_SEARCH_DETECTION",
-                                    'maxResults': 10
-                                    }
-                                    ]
-                       }
-                       ]
-                       }
+      str_json_data = {
+            'requests': [
+                         {
+                         'image': {
+                         'content': str_encode_file
+                         },
+                         'features': [
+                                      {
+                                      'type': "SAFE_SEARCH_DETECTION",
+                                      'maxResults': 10
+                                      }
+                                      ]
+                         }
+                         ]
+                         }
 
-    # リクエスト送信
-    obj_session = Session()
-    obj_request = Request("POST",str_url + str_api_key,
-                                        data=json.dumps(str_json_data),
-                                        headers=str_headers
-                                        )
-    obj_prepped = obj_session.prepare_request(obj_request)
-    obj_response = obj_session.send(obj_prepped,verify=True,timeout=60)
-          
-      # 分析結果の取得
-    if obj_response.status_code == 200:
-      print obj_response.text
-      jsonData = json.loads(obj_response.text)
-      params = jsonData["responses"][0]["safeSearchAnnotation"]
-      if params["adult"] == "VERY_LIKELY":
-        print "out"
-      if params["adult"] == "LIKELY":
-        print "out"
-      if params["adult"] == "POSSIBLE":
-        print "seout"
+      # リクエスト送信
+      obj_session = Session()
+      obj_request = Request("POST",str_url + str_api_key,
+                                          data=json.dumps(str_json_data),
+                                          headers=str_headers
+                                          )
+      obj_prepped = obj_session.prepare_request(obj_request)
+      obj_response = obj_session.send(obj_prepped,verify=True,timeout=60)
+            
+        # 分析結果の取得
+      if obj_response.status_code == 200:
+        print obj_response.text
+        jsonData = json.loads(obj_response.text)
+        params = jsonData["responses"][0]["safeSearchAnnotation"]
+        if params["adult"] == "VERY_LIKELY":
+          print "out"
+        if params["adult"] == "LIKELY":
+          print "out"
+        if params["adult"] == "POSSIBLE":
+          print "seout"
 
-        #return obj_response.text
-    else:
-      #return "error"
-      print "error"  
+          #return obj_response.text
+      else:
+        #return "error"
+        print "error"  
 
-    time.sleep(5)
-    #break
+      #break
 
 capture.release()
 cv2.destroyAllWindows()#事後処理
